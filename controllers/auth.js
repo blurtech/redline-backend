5a3b5335590c909ab731e0f43c3d27b386847578
MacOSO <auroragamesproject@gmail.com>
2019-05-18 19:44:16 +0700

    Added customResponses

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,1 +1,6 @@
+const repository = require('../repositories/users');
 
+exports.sendMessage = async (req, res) => {
+    const data = await repository.getKeywords();
+    return res.success({message: answer.message});
+};
