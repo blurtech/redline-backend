017c5ce9db267881a1d07a415ee2d9e16c8a5dbe
MacOSO <auroragamesproject@gmail.com>
2019-05-19 00:26:30 +0700

    Fix registration

--- repositories/users.js
+++ repositories/users.js
@@ -2,6 +2,6 @@ const Users = require('../models/user');
 
 exports.getUserById = (id) => Users.findById( id, {'password': 0} );
 
-exports.register = (login, password, name) => Users.create({'login': login, 'password': password, 'name': name});
+exports.register = (login, password, name, preferences) => Users.create({login: login, password: password, name: name, preferences: preferences});
 
 exports.authorize = (login, password) => Users.findOne( {'login': login, 'password': password}, {'_id': 1} );
