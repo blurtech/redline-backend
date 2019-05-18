bf372e0a3ce17e3c899c88b0fc33e72311bb36e7
MacOSO <auroragamesproject@gmail.com>
2019-05-18 16:32:31 +0700

    Init project

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express'),
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    logger = require('morgan');
+
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+
+let app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
