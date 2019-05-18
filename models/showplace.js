450a422fb90f1f2d29798c7b2c874656e7e7aea8
AlexBulatov <al.ross@yandex.ru>
2019-05-18 18:28:33 +0700

    Added showplace model

---
+++ models/showplace.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let showplaceSchema = new Schema({
+    id: Schema.ObjectId,
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    geo: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    }
+});
