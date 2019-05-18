d534c1b968f1d0459ca9ea65a71718f8ba77c3a8
Puzovoz <puzovoz@gmail.com>
2019-05-18 19:54:00 +0700

    Changed auth in users repository

--- controllers/showplaces.js
+++ controllers/showplaces.js
@@ -1,2 +1,6 @@
 const repository = require('../repositories/showplaces');
 
+exports.sendShowplaces = async (req, res) => {
+    const showplaces = await repository.getShowplaces();
+    res.success(showplaces);
+}
