cd9b895bf0a46049e30865faffc4b3305888c806
AlexBulatov <al.ross@yandex.ru>
2019-05-19 02:29:33 +0700

    Removed create method

--- repositories/routes.js
+++ repositories/routes.js
@@ -5,15 +5,3 @@ exports.getRedLines = (city) => Routes.find( {'city': city});
 exports.getCities = () => Routes.find().distinct('city', {});
 
 exports.getAllRoutes = () => Routes.find({});
-
-exports.createRoute = (name, description, routes) => { //Routes - массив showplace
-    let newRoute = new Routes({
-        "name": name,
-        "description": description,
-        "geos": routes
-    });
-
-    newRoute.save(function (err) {
-        if (err) return err;
-    });
-};
