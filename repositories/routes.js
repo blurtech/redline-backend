95a539eeb01d163c951c1478a1dc1d448d0f76e7
MacOSO <auroragamesproject@gmail.com>
2019-05-19 02:18:30 +0700

    Remove ids

--- repositories/routes.js
+++ repositories/routes.js
@@ -1,6 +1,6 @@
 const Routes = require('../models/route');
 
-exports.getRedLines = (city) => Routes.find( {'city': city}, {_id: 1, name: 1, city: 1, description: 1, geo: 1, address: 1, tags: 1} );
+exports.getRedLines = (city) => Routes.find( {'city': city}, {name: 1, city: 1, description: 1, geo: 1, address: 1, tags: 1} );
 
 exports.getCities = () => Routes.find().distinct('city', {});
 
