22c9f579a3a91f7571ce6ef0cac01009680c83e2
MacOSO <auroragamesproject@gmail.com>
2019-05-19 04:32:56 +0700

    Return user model

--- repositories/users.js
+++ repositories/users.js
@@ -4,4 +4,4 @@ exports.getUserById = (id) => Users.findById( id, {'password': 0} );
 
 exports.register = (login, password, name, preferences) => Users.create({login: login, password: password, name: name, preferences: preferences});
 
-exports.authorize = (login, password) => Users.findOne( {'login': login, 'password': password}, {_id: 1} );
+exports.authorize = (login, password) => Users.findOne( {'login': login, 'password': password}, {password: 0} );
